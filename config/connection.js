9e49cfe3b387d06ad4f20c525cc816d430897e43
Ikra Rafi <irafi@nlihc.org>
2021-02-26 11:27:44 -0500

    jawsdb added

--- config/connection.js
+++ config/connection.js
@@ -2,8 +2,8 @@
 var mysql = require('mysql');
 var connection;
 
-if (process.env.JAWS_URL) {
-  connection = mysql.createConnection(process.env.JAWS_URL);
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
 connection = mysql.createConnection({
   host: 'localhost',
